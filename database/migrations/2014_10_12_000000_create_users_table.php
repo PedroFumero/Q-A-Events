3c315cb5851cf10b986da16fd037bd3464b74844
Pedro Fumero <mail@pedrofumero.com>
2021-04-12 14:13:03 -0400

    Migration and seed of role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreign('FK_ROLE')->references('id')->on('roles')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
